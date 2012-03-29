307fb90ee01cc84110aa40c84021139af6beb96a
Natan Shavla <natan.shalva@gmail.com>
2012-03-29 23:40:38 +0300

    m

--- basic/project/index.php
+++ basic/project/index.php
@@ -1,12 +1,47 @@
-<?php 
-  session_start();
+<?php
+include('./includes/db_connection.php');
+include('./includes/function.php');
+include('./includes/html/html_header.php');
 ?>
-<?php  
+<header>
+  <h1>this is the page header</h1>
+</header>
+<article>
+  <aside>
+    <ul>
+      <?php
+      $query = " SELECT * ";
+      $query .= " FROM subject ";
+    //  $query .= " WHERE subject ";
+      
+      $subject_set = mysql_query($query);
 
-include('./includes/html/html_header.php') ;
+      // get the subject 
+      while ($subject = mysql_fetch_array($subject_set)) {
+        echo "<li>" . $subject['subject'] . "</li>";
+          echo "<ul>";
+          
+        // get the pages
+        $query = "SELECT * FROM pages";
+        $query .=  " WHERE  subject_id = '{$subject['id']}' ";
+
+        $result = mysql_query($query , $connection);
+        
+        while ($pages = mysql_fetch_array($result)) {
+
+          echo "<li><a href='content.php?id={$pages['id']}'>" . $pages['title'] . "</a></li>";
+        }
+        echo "</ul>";
+      }
+      ?>  
+    </ul>
+  </aside>
+
+
+
+</article>
+
+
+<?php
+include('./includes/html/footer.php');
 ?>
-<?php   
-$_SESSION['name'] = 'natan2';
-?> 
-</body>
-</html>
